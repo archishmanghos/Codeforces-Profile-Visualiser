74656533a77ce0e243df70c5205bfb94d3d1f622
Archishman Ghosh <ag2324@it.jgec.ac.in>
2022-11-16 19:00:31 +0530

    Finished and wrapped things up

--- app.js
+++ app.js
@@ -37,16 +37,25 @@ app.get('/login', (req, res) => {
 
         response.on('end', () => {
             const jsonData = JSON.parse(data);
+            const handle = jsonData.result[0].handle;
+            const dp = jsonData.result[0].titlePhoto;
+            const firstName = typeof jsonData.result[0].firstName !== 'undefined' ? jsonData.result[0].firstName : 'Anonymous';
+            const lastName = typeof jsonData.result[0].lastName !== 'undefined' ? jsonData.result[0].lastName : '';
+            const rating = jsonData.result[0].maxRating + '(' + jsonData.result[0].maxRank + ')';
+            const friends = jsonData.result[0].friendOfCount;
+            const place = typeof jsonData.result[0].city !== 'undefined' ? jsonData.result[0].city : '' + ', ' + typeof jsonData.result[0].country !== 'undefined' ? jsonData.result[0].country : 'Somewhere on Earth';
+            const org = jsonData.result[0].organization.length ? jsonData.result[0].organization : 'OR-chi Fan Club :p';
+
             if (jsonData.status === 'OK') {
                 res.render('login', {
-                    handle: jsonData.result[0].handle,
-                    dp: jsonData.result[0].titlePhoto,
-                    firstName: jsonData.result[0].firstName,
-                    lastName: jsonData.result[0].lastName,
-                    rating: jsonData.result[0].maxRating + '(' + jsonData.result[0].maxRank + ')',
-                    friends: jsonData.result[0].friendOfCount,
-                    place: jsonData.result[0].city + ', ' + jsonData.result[0].country,
-                    org: jsonData.result[0].organization
+                    handle: handle,
+                    dp: dp,
+                    firstName: firstName,
+                    lastName: lastName,
+                    rating: rating,
+                    friends: friends,
+                    place: place,
+                    org: org
                 });
             } else {
                 res.render('failure');
@@ -56,71 +65,33 @@ app.get('/login', (req, res) => {
 });
 
 app.post('/login', (req, res) =>{
-    const buttonClicked = req.body.button;
-    if (buttonClicked === 'subButton') {
-        https.get(statusUrl, (response) => {
-            let data = '';
-            response.on('data', (chunk) => {
-                data += chunk;
-            });
-    
-            response.on('end', () => {
-                const jsonData = JSON.parse(data);
-                if (jsonData.status === 'OK') {
-                    let ratings = new Array(10).fill(0);
-                    let allRatings = [1200, 1400, 1600, 1900, 2100, 2300, 2400, 2600, 3000, 4000];
-                    for (let i = 0; i < jsonData.result.length; i++) {
-                        for (let j = 0; j < 10; j++) {
-                            if (jsonData.result[i].problem.rating < allRatings[j]) {
-                                ratings[j] += 1;
-                            }
+    https.get(statusUrl, (response) => {
+        let data = '';
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        response.on('end', () => {
+            const jsonData = JSON.parse(data);
+            if (jsonData.status === 'OK') {
+                let ratings = new Array(10).fill(0);
+                let allRatings = [1200, 1400, 1600, 1900, 2100, 2300, 2400, 2600, 3000, 4000];
+                for (let i = 0; i < jsonData.result.length; i++) {
+                    for (let j = 0; j < 10; j++) {
+                        if (jsonData.result[i].problem.rating < allRatings[j]) {
+                            ratings[j] += 1;
                         }
                     }
-                    
-                    res.render('submissions', {ratings: ratings, allRatings: allRatings});
-                } else {
-                    res.render('failure');
                 }
-            });
+
+                res.render('submissions', {ratings: ratings, allRatings: allRatings});
+            } else {
+                res.render('failure');
+            }
         });
-    } else {
-        https.get(ratingUrl, (response) => {
-            let data = '';
-            response.on('data', (chunk) => {
-                data += chunk;
-            });
-    
-            response.on('end', () => {
-                const jsonData = JSON.parse(data);
-                // console.log(jsonData);
-                if (jsonData.status === 'OK') {
-                    res.render('rating')
-                } else {
-                    res.render('failure');
-                }
-            });
-        });
-    }
+    });
 });
 
 app.listen(PORT, () => {
     console.log('Server running on port 3000');
 });
-
-// handle: jsonData.result[0].handle,
-// dp: jsonData.result[0].titlePhoto,
-// firstName: jsonData.result[0].firstName,
-// lastName: jsonData.result[0].lastName,
-// rating: jsonData.result[0].maxRating + '(' + jsonData.result[0].maxRank + ')',
-// friends: jsonData.result[0].friendOfCount,
-// place: jsonData.result[0].city + ', ' + jsonData.result[0].country,
-// org: jsonData.result[0].organization
-
-// handle: 'OR-chi',
-// dp: '..',
-// firstName: 'Archishman',
-// lastName: 'Ghosh',
-// rating: '1561' + '(' + 'Specialist' + ')',
-// friends: '116',
-// place: 'Kolkata, India',
-// org: 'JGEC'
